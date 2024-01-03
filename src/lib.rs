50afd9e05b58d47219d285656dcc085b3b971e43
konomae <konomae@users.noreply.github.com>
2024-01-03 17:01:13 +0900

    implement register_tool

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,14 @@
 use extism_pdk::*;
 use proto_pdk::*;
+
+static NAME: &str = "Zig";
+
+#[plugin_fn]
+pub fn register_tool(_: ()) -> FnResult<Json<ToolMetadataOutput>> {
+    Ok(Json(ToolMetadataOutput {
+        name: NAME.into(),
+        type_of: PluginType::Language,
+        plugin_version: Some(env!("CARGO_PKG_VERSION").into()),
+        ..ToolMetadataOutput::default()
+    }))
+}
