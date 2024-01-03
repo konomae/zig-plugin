11f4ef1351345644cc1b34c2b05bad44b11e1d87
konomae <konomae@users.noreply.github.com>
2024-01-03 17:35:08 +0900

    implement download_prebuilt

--- src/lib.rs
+++ src/lib.rs
@@ -12,3 +12,62 @@ pub fn register_tool(_: ()) -> FnResult<Json<ToolMetadataOutput>> {
         ..ToolMetadataOutput::default()
     }))
 }
+
+#[plugin_fn]
+pub fn download_prebuilt(
+    Json(input): Json<DownloadPrebuiltInput>,
+) -> FnResult<Json<DownloadPrebuiltOutput>> {
+    let env = get_proto_environment()?;
+
+    check_supported_os_and_arch(
+        NAME,
+        &env,
+        permutations! [
+            HostOS::Linux => [HostArch::X86, HostArch::X64, HostArch::Arm64],
+            HostOS::MacOS => [HostArch::X64, HostArch::Arm64],
+            HostOS::Windows => [HostArch::X86, HostArch::X64, HostArch::Arm64],
+        ],
+    )?;
+
+    let version = input.context.version;
+
+    let arch = match env.arch {
+        HostArch::X86 => "x86",
+        HostArch::X64 => "x86_64",
+        HostArch::Arm64 => "aarch64",
+        _ => unreachable!(),
+    };
+
+    let os = env.os;
+
+    let prefix = match os {
+        HostOS::Linux => format!("zig-linux-{arch}-{version}"),
+        HostOS::MacOS => format!("zig-macos-{arch}-{version}"),
+        HostOS::Windows => format!("zig-windows-{arch}-{version}"),
+        _ => unreachable!(),
+    };
+
+    let filename = if os == HostOS::Windows {
+        format!("{prefix}.zip")
+    } else {
+        format!("{prefix}.tar.xz")
+    };
+
+    let directory = match &version {
+        VersionSpec::Version(v) if !v.pre.is_empty() => "builds".to_string(),
+        _ => format!("downloads/{version}"),
+    };
+
+    Ok(Json(DownloadPrebuiltOutput {
+        archive_prefix: Some(prefix),
+        checksum_url: Some(format!(
+            "https://ziglang.org/{directory}/{filename}.minisig"
+        )),
+        checksum_public_key: Some(
+            "RWSGOq2NVecA2UPNdBUZykf1CCb147pkmdtYxgb3Ti+JO/wCYvhbAb/U".into(),
+        ),
+        download_url: format!("https://ziglang.org/{directory}/{filename}"),
+        download_name: Some(filename),
+        ..DownloadPrebuiltOutput::default()
+    }))
+}
